2a22e0b586387544b07b9ce9da449f9dffb79799
camiloperez17 <a17campermor@inspedralbes.cat>
2022-05-02 10:36:08 +0200

    Register

--- web/vue-peluqueria/src/components/register.vue
+++ web/vue-peluqueria/src/components/register.vue
@@ -43,7 +43,7 @@
                 nombre: '',
                 apellido: '',
                 email: '',
-                contrasena
+                contrasena: ''
             }
         },
         methods: {
