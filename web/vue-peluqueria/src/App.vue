341ecdc360faceccdcca6996a2420728c2d4b257
a18dylvargar <a18dylvargar@inspedralbes.cat>
2022-05-02 12:32:20 +0200

    problema boost

--- web/vue-peluqueria/src/App.vue
+++ web/vue-peluqueria/src/App.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "@/components/HelloWorld.vue";
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
 </script>
 
 
@@ -9,6 +13,25 @@ import HelloWorld from "@/components/HelloWorld.vue";
       <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+     <div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
       </nav>
   </header>
 
