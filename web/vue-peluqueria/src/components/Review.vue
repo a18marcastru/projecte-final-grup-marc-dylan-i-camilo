32a293fa6e1a42d815262ad483264706896ba9e3
a18dylvargar <a18dylvargar@inspedralbes.cat>
2022-05-13 13:03:58 +0200

    coment and review

---
+++ web/vue-peluqueria/src/components/Review.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">{{infoComent.nombre}}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">{{infoComent.apellido}}</h6>
+    <p class="card-text">{{infoComent.descripcion}}</p>
+    <a href="#" class="card-link">{{infoComent.valoracion}}</a>
+  </div>
+</div>
+</template>
+
+<script>
+export default{
+    props: ['infoComent']
+}
+
+</script>
+<style>
+
+
+
+</style>
