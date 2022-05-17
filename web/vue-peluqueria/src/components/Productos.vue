212879df855156182ea4c35d87ff9a8d0224d955
camiloperez17 <a17campermor@inspedralbes.cat>
2022-05-17 13:18:45 +0200

    tienda

--- web/vue-peluqueria/src/components/Productos.vue
+++ web/vue-peluqueria/src/components/Productos.vue
@@ -1,12 +1,16 @@
 <template>
-        <div class="card" style="width: 18rem;">
-        <div class="card-body">
+    <div class="card grid-container" style="width: 18rem;">
+        <div class="card-body item">
             <h5 class="card-title">{{infoProduct.nombre}}</h5>
             <h6 class="card-subtitle mb-2 text-muted">{{infoProduct.descripcion}}</h6>
             <p class="card-text">Cantidad :  {{infoProduct.cantidad}}</p>
             <p class="card-text">Precio : {{infoProduct.precio}} €  </p>
+            <button class="agregar">+</button><input type="text" value="0"><button class="quitar">-</button>
         </div>
-        </div>
+    </div>
+    <div class="card-body item">
+        
+    </div>
 </template>
 
 <script>
@@ -14,3 +18,13 @@ export default {
     props: ['infoProduct'],
 }
 </script>
+
+<style>
+    .grid-container {
+        display: grid;
+        grid-template-columns: auto auto auto;
+    }
+    .item {
+
+    }
+</style>
