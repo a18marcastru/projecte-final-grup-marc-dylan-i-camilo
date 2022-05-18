147a673a28ae88c9541285fd52ab1fd076e94486
a18marcastru <a18marcastru@inspedralbes.cat>
2022-05-18 12:20:56 +0200

    Tienda

--- web/vue-peluqueria/src/components/Tienda.vue
+++ web/vue-peluqueria/src/components/Tienda.vue
@@ -1,22 +1,28 @@
 <template>
   <h1>Tienda de peluqueria</h1>
-  <div id="producto">
-  <div v-for="ses in datos">
-    <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">{{ses.nombre}}</h5>
-        <!-- <img :src="infoProduct.imagen"> -->
-        <p class="card-subtitle mb-2 text-muted">{{ses.descripcion}}</p>
-        <p class="card-text">Stock :  {{ses.cantidad}}</p>
-        <p class="card-text">Precio : {{ses.precio}} €</p>
-        <button @click="sumar(ses.nombre, ses.cantidad)">+</button>
-        <input type="text" :id="ses.nombre" value="0" />
-        <button @click="restar(ses.nombre)">-</button>
+  <div id="container">
+    <div id="producto">
+      <div v-for="ses in datos">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">{{ses.nombre}}</h5>
+            <!-- <img :src="infoProduct.imagen"> -->
+            <p class="card-subtitle mb-2 text-muted">{{ses.descripcion}}</p>
+            <p class="card-text">Stock :  {{ses.cantidad}}</p>
+            <p class="card-text">Precio : {{ses.precio}} €</p>
+            <button @click="sumar(ses.nombre, ses.cantidad, ses.precio)">+</button>
+            <input type="text" :id="ses.nombre" value="0" />
+            <button @click="restar(ses.nombre)">-</button>
+          </div>
+        </div>
       </div>
     </div>
+    <div id="caruaje">
+      <h2>Lista de productos:</h2>
+      <div id="lista"></div>
+      <button class="btn btn-primary" id="btn-comprar" @click="comprar()" disabled>Comprar</button>
+    </div>
   </div>
-  </div>
-  <button @click="comprar()">Comprar</button>
 </template>
 
 <script>
@@ -27,7 +33,9 @@
         datos: [],
         total: [],
         email: '',
-        precio_total: ''
+        precio_total: 0,
+        stocks_total: 0,
+        stock_groups: {}
       }
     },
     components: {
@@ -45,7 +53,7 @@
       comprar() {
         console.log("Quiero comprar esto: " + this.seleccion);
       },
-      sumar(nombre, stock) {
+      sumar(nombre, stock, precio) {
         console.log(nombre);
         let num = document.getElementById(nombre).value;
         if(num < stock) {
@@ -53,48 +61,61 @@
           document.getElementById(nombre).value = num;
           let num_total = num;
           let encontrado = false;
-          for(let i = 0; i < this.total.length ;i++) {
+          for (let i = 0; i < this.total.length ;i++) {
             //si lo he encontrado, le sumo uno
-            if(this.total[i].nombre == nombre) {
+            if (this.total[i].nombre == nombre) {
               this.total[i].cantidades = num_total;
               encontrado = true;
             }            
           }
-          if (encontrado==false){
+          if (encontrado == false){
             this.total.push({'nombre':nombre, 'cantidades': num_total});
           }
           console.log(this.total);
+          this.actualizar(nombre, num_total, precio);
         }
         else {
           alert("No hay suficiente stock");
         }
       },
-      restar(nombre) {
+      actualizar(nombre, num_total, precio){
+        console.log(num_total);
+        let htmlstr = "";
+        for(let i = 0;i < this.total.length;i++) {
+          if(this.total[i].cantidades != 0) {
+            htmlstr += `<h3>Aticulo: ${i+1}</h3><p class>Nombre del producto: ${this.total[i].nombre}</p>
+                        <p>Cantidades: ${this.total[i].cantidades}`;
+            this.stock_groups[nombre] = precio * num_total;
+          }
+        }
+        console.log(this.stock_groups);
+        this.precio_total = Object.values(this.stock_groups).reduce((acc,curr) => acc + curr,0);
+        htmlstr += "<hr/>";
+        htmlstr += `<p>Precio total: ${this.precio_total}</p>`;
+        document.getElementById("lista").innerHTML = htmlstr;
+        document.getElementById("btn-comprar").removeAttribute("disabled");
+      },
+      restar(nombre, precio) {
         let num = document.getElementById(nombre).value;
         if(num > 0) {
           num--;
           document.getElementById(nombre).value = num;
           let num_total = num;
-          let encontrado = "";
           let index = "";
           for(let i = 0; i < this.total.length ;i++) {
-            //si lo he encontrado, le sumo uno
+            //si lo he encontrado, le resto uno
             if(this.total[i].nombre == nombre) {
-              if(this.total[i].cantidades != 0) {
+              if(num_total != 0) {
                 this.total[i].cantidades = num_total;
-                encontrado = true;
               }
               else {
-                console.log("Hola");
-                encontrado = false;
-                let index = i;
+                this.total[i].cantidades = num_total;
+                this.total = this.total.filter(can => can.cantidades > 0);
               }
             }            
           }
-          if(encontrado == false){
-            this.total[index].splice();
-          }
           console.log(this.total);
+          this.actualizar(nombre, num_total, precio)
         }
         else if(num == 0) {
           alert("No puedes restar mas");
@@ -102,12 +123,11 @@
       },
       comprar() {
         console.log(this.total);
-        this.email = "marc@gmail.com";
-        this.precio_total = 32;
+        this.email = "marc@gmail.com";;
         const productos = JSON.stringify(this.total);
         const datosEnvio = new FormData();
         datosEnvio.append('email', this.email);
-        datosEnvio.append('productos', this.productos);
+        datosEnvio.append('productos', productos);
         datosEnvio.append('precio_total', this.precio_total);
         
         fetch('http://192.168.210.154:8000/tickets/nuevo/ticket', {
@@ -127,8 +147,16 @@
   h1 {
     text-align: center;
   }
+  #container {
+    display: flex;
+  }
   #producto {
     display: grid;
     grid-template-columns: auto auto;
+  }
+  #caruaje {
+    margin-left: 30%;
+    padding: 5px;
+    border: 5px solid black;
   }
 </style>
